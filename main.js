c94a9446f03fefcc172bd10f7581627346741351
MedhaManasvini <79033962+MedhaManasvini@users.noreply.github.com>
2021-09-04 22:00:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function openComputer(){
+    window.location= "computer.html"
+}
+function openMathematics(){
+    window.location= "maths.html"
+}
+function openScience(){
+    window.location= "science.html"
+}
+function openEnglish(){
+    window.location= "english.html"
+}
+function openSSt(){
+    window.location= "sst.html"
+}
+function openHindi(){
+    window.location= "computer.html"
+}
+function openSanskrit(){
+    window.location= "computer.html"
+}
+function openCoding(){
+    window.location= "coding.html"
+}
+function openPiano(){
+    window.location= "piano.html"
+}
+function openDance1(){
+    window.location= "dance1.html"
+}
+function openDance2(){
+    window.location= "dance2.html"
+}
